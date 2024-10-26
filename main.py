efa0d62888c98dda8965d6a5cec672e13c728ded
Bohdan Pavliukh <bpavliukh.ioc@gmail.com>
2024-10-26 10:17:27 +0300

    test push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import model
 import inference_mamdani
 import fuzzy_operators
 
-crisp = [0.2, 0.35, 0.63]
+crisp = [0.2, 0.45, 0.63]
 
 inference_mamdani.preprocessing(model.input_lvs, model.output_lv)
 result = inference_mamdani.process(model.input_lvs, model.output_lv, model.rule_base, crisp)
